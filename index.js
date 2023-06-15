c59f1de048882e5c2424e657269f77c55c0a5f48
ururu2407 <122101396+ururu2407@users.noreply.github.com>
2023-06-15 21:59:52 +0300

    second

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { handleValidationErrors, checkAuth } from './utils/index.js';
 import { UserController, PostController } from './controllers/index.js';
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect("mongodb+srv://aklokun2407:kozadenuwtak@cluster0.qprfrz8.mongodb.net/blog?retryWrites=true&w=majority")
   .then(() => console.log('DB ok'))
   .catch((err) => console.log('DB error', err));
 
@@ -61,7 +61,7 @@ app.patch(
   PostController.update,
 );
 
-app.listen(process.env.PORT || 4444, (err) => {
+app.listen(4444, (err) => {
   if (err) {
     return console.log(err);
   }
